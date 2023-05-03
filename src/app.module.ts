540f306cf170115aff4ef51cbf9a35c378852d3a
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2023-05-03 20:16:54 -0300

    config acess DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DotenvConfigOutput, config } from 'dotenv';
 import { TaskModule } from './task/task.module';
 import { Task } from './task/task.entity';
 
 @Module({
   imports: [
     TaskModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'tasks',
-      entities: [Task],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (): any => {
+        const envConfig: DotenvConfigOutput = config();
+
+        return {
+          type: 'mysql',
+          host: envConfig.parsed.DB_HOST,
+          port: +envConfig.parsed.DB_PORT,
+          username: envConfig.parsed.DB_USERNAME,
+          password: envConfig.parsed.DB_PASSWORD,
+          database: envConfig.parsed.DB_NAME,
+          entities: [Task],
+          synchronize: true,
+        };
+      },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
